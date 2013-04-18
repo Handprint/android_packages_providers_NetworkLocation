6e052575611e37365d318c355b3023ff1a33498c
Jonathan Harlap <jharlap@gmail.com>
2013-04-18 13:14:28 -0400

    renaming to FreeNetworkLocation

--- NetworkLocation/Android.mk
+++ NetworkLocation/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-proto-files-unde
 
 LOCAL_JAVA_LIBRARIES := com.android.location.provider android-support-v4
 
-LOCAL_PACKAGE_NAME := NetworkLocation
+LOCAL_PACKAGE_NAME := FreeNetworkLocation
 LOCAL_CERTIFICATE := platform
 LOCAL_SDK_VERSION := current
 
