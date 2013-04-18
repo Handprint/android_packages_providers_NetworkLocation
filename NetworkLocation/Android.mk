966b0f9f67a3be8144aa1fdd2baead4d6173b3fd
Jonathan Harlap <jharlap@gmail.com>
2013-04-18 09:50:57 -0400

    including protobuf

--- NetworkLocation/Android.mk
+++ NetworkLocation/Android.mk
@@ -7,10 +7,18 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-proto-files-unde
 
 LOCAL_JAVA_LIBRARIES := com.android.location.provider
 
-LOCAL_STATIC_JAVA_LIBRARIES := libprotobuf-java-2.3.0-lite
+LOCAL_STATIC_JAVA_LIBRARIES := libprotobuf-lite
 
 LOCAL_PACKAGE_NAME := FreeNetworkLocation
 LOCAL_CERTIFICATE := platform
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
+
+#############################
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libprotobuf-lite:libs/protobuf-java-2.5.0.jar
+
+include $(BUILD_MULTI_PREBUILT)
