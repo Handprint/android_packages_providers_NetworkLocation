07085eef5170bb9cd885708c69e18b72eee73227
Jonathan Harlap <jharlap@gmail.com>
2013-04-18 10:27:34 -0400

    using android-support

--- NetworkLocation/Android.mk
+++ NetworkLocation/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-proto-files-unde
 LOCAL_JAVA_LIBRARIES := com.android.location.provider
 
 LOCAL_STATIC_JAVA_LIBRARIES := libprotobuf-lite
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 
 LOCAL_PACKAGE_NAME := FreeNetworkLocation
 LOCAL_CERTIFICATE := platform
